a0f1ffeec789d71b5580545431171b0098997214
Isanka-Suresh <isankasuresh001@gmail.com>
2026-08-09 15:39:26 +0530

    feat: initialize Strapi project with Post, Author, Category, and Contact content types

---
+++ dist/config/middlewares.js
@@ -0,0 +1,40 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = [
+    'strapi::logger',
+    'strapi::errors',
+    {
+        name: 'strapi::security',
+        config: {
+            contentSecurityPolicy: {
+                useDefaults: true,
+                directives: {
+                    'connect-src': ["'self'", 'https:'],
+                    'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+                    'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+                    upgradeInsecureRequests: null,
+                },
+            },
+        },
+    },
+    {
+        name: 'strapi::cors',
+        config: {
+            enabled: true,
+            headers: '*',
+            origin: [
+                'http://localhost:3000',
+                'http://localhost:1337',
+                process.env.FRONTEND_URL || 'http://localhost:3000',
+                process.env.PUBLIC_URL || 'http://localhost:1337',
+            ],
+        },
+    },
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+];
+exports.default = config;
